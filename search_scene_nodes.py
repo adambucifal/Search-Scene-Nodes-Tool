eab2cf8a202c4909a0952e196873868b68eec0cd
Adam <145237957+adambucifal@users.noreply.github.com>
2024-05-22 18:27:51 -0700

    Add files via upload

--- search_scene_nodes.py
+++ search_scene_nodes.py
@@ -65,6 +65,7 @@ class SearchSceneNodes(QtWidgets.QDialog):
 			self.setWindowFlag(QtCore.Qt.Tool, True)
 		
 		self.scene_nodes = cmds.ls()
+		#self.scene_shapes = cmds.ls(shapes=True)
 		
 		self.create_widgets()
 		self.create_connections()
@@ -80,6 +81,7 @@ class SearchSceneNodes(QtWidgets.QDialog):
 		:return: None
 		"""
 		self.search_le = QtWidgets.QLineEdit()
+		self.no_shapes_checkbox = QtWidgets.QCheckBox("Exclude Shapes")
 		
 		# Filter by node type combo box
 		self.nodetype_cb = QtWidgets.QComboBox()
@@ -101,14 +103,18 @@ class SearchSceneNodes(QtWidgets.QDialog):
 		
 		:return: None
 		"""
-		grid_layout = QtWidgets.QGridLayout()
-		grid_layout.addWidget(self.nodetype_cb, 0, 0)
-		grid_layout.addWidget(self.clear_nodetype_btn, 0, 1)
-		grid_layout.addWidget(self.reload_nodes_btn, 0, 2)
+		nodetype_grid_layout = QtWidgets.QGridLayout()
+		nodetype_grid_layout.addWidget(self.nodetype_cb, 0, 0)
+		nodetype_grid_layout.addWidget(self.clear_nodetype_btn, 0, 1)
+		nodetype_grid_layout.addWidget(self.reload_nodes_btn, 0, 2)
+		
+		search_grid_layout = QtWidgets.QGridLayout()
+		search_grid_layout.addWidget(self.search_le, 0, 0)
+		search_grid_layout.addWidget(self.no_shapes_checkbox, 0, 1)
 		
 		form_layout = QtWidgets.QFormLayout()
-		form_layout.addRow("Filter node type", grid_layout)
-		form_layout.addRow("Search scene", self.search_le)
+		form_layout.addRow("Filter node type", nodetype_grid_layout)
+		form_layout.addRow("Search nodes", search_grid_layout)
 		form_layout.addRow(self.display_nodes_lw)
 		
 		main_layout = QtWidgets.QVBoxLayout(self)
@@ -125,9 +131,10 @@ class SearchSceneNodes(QtWidgets.QDialog):
 		self.display_nodes_lw.itemSelectionChanged.connect(self.item_selection_changed)
 		self.nodetype_cb.currentIndexChanged.connect(self.update_node_type)
 		self.clear_nodetype_btn.clicked.connect(
-			lambda: search_scene_nodes_win.nodetype_cb.setCurrentIndex(0)
+			lambda: self.nodetype_cb.setCurrentIndex(0)
 		)
 		self.reload_nodes_btn.clicked.connect(self.reload_scene_nodes)
+		self.no_shapes_checkbox.clicked.connect(self.exclude_shapes_changed)
 	
 	
 	# Utility methods
@@ -141,6 +148,15 @@ class SearchSceneNodes(QtWidgets.QDialog):
 		"""
 		current_sel_node_type = self.nodetype_cb.itemText(index)
 		self.filter_node_type(current_sel_node_type)
+	
+	
+	def exclude_shapes_changed(self) -> None:
+		"""
+		Updates the display nodes when exclude shapes is modified
+		
+		:return: None
+		"""
+		self.update_display_nodes()
 
 	
 	def filter_node_type(self, node_type: str) -> None:
@@ -167,8 +183,7 @@ class SearchSceneNodes(QtWidgets.QDialog):
 		
 		:return: None
 		"""
-		node_type = self.nodetype_cb.currentText()
-		self.filter_node_type(node_type)
+		self.filter_node_type(self.nodetype_cb.currentText())
 	
 	
 	def update_display_nodes(self) -> None:
@@ -177,12 +192,24 @@ class SearchSceneNodes(QtWidgets.QDialog):
 		
 		:return: None
 		"""
+		# Get user filter
 		filter_string = self.search_le.text()
-
-		self.display_nodes_lw.clear()
-		self.display_nodes_lw.addItems([
+		
+		# Find nodes with matching strings
+		nodes = [
 			node for node in self.scene_nodes if filter_string.lower() in node.lower()
-		])
+			]
+		
+		# Get rid of shape nodes if the box is checked
+		if self.no_shapes_checkbox.isChecked():
+			scene_shapes = cmds.ls(shapes=True)
+			nodes = [
+				node for node in nodes if node not in scene_shapes
+			]
+		
+		# Clear and re add new nodes
+		self.display_nodes_lw.clear()
+		self.display_nodes_lw.addItems(nodes)
 	
 	
 	def item_selection_changed(self) -> None:
@@ -198,6 +225,7 @@ class SearchSceneNodes(QtWidgets.QDialog):
 				if cmds.objExists(item.text())
 			]
 		)
+
 
 if __name__ == "__main__":
 	
